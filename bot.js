c994ee50eb7e9ca6df34d94215cd010e905a899c
powellgatwit <powellg@wit.edu>
2022-04-09 21:06:04 -0400

    Now supports multiple servers

--- bot.js
+++ bot.js
@@ -16,14 +16,15 @@ client.once('ready', () => {
 client.login(token);
 
 
-function dailymessage() {
+function dailymessage(channelInstance) {
 
 	let api = 'https://orthocal.info/api/oca/';
 	let george = 'https://orthocal.info/api/oca/2022/4/23/';
 
 	https.get(api, (result) => {
 		let data = "";
-		console.log('statusCode:', result.statusCode);
+		//to test connection with OrthoCal API:
+		//console.log('statusCode:', result.statusCode);
 
 		result.on('data', (d) => {
 			data = data + d.toString();
@@ -31,33 +32,31 @@ function dailymessage() {
 		result.on('end', () => {
 			const body = JSON.parse(data);
 
-			/* 	Title, feasts	*/
+			// creates a new string 'msg' that is expanded with each attribute of the daily calendar
 
+			//title, feasts
 			var msg = new String(
 				"Good morning! Today is the " + body.titles + ".\n"
 			);
 
 			if (body.feasts != null) {
-
 				msg = msg.concat(
 					"It is the feast of the " + body.feasts + ".\n"
 				);
 			}
 
-			/*		Saints	 */
-
+			//saints
 			if (body.saints != null) {
 				msg = msg.concat(
 					"On this day we commemorate (the) " + body.saints + ".\n"
 				);
 			}
 
-			/* 	Fasting	*/
-
+			//fasting
 			if (body.fast_level > 0) {
 				msg = msg.concat("> Today is a fasting day.\n");
 
-				//name of fast, if there is one
+				//...and name of fast, if there is one
 				if (body.fast_level > 1) {
 					msg = msg.concat(
 						"> We are in the " + body.fast_level_desc + ".\n"
@@ -70,15 +69,13 @@ function dailymessage() {
 						"> *Today's note: " + body.fast_exception_desc + ".*"
 					);
 				}
-			}
+			} //okay, no more fasting stuff
 
-			//const channel = client.channels.cache.get('850364310319661109');
-
-			const channel = client.channels.cache.find(ch => ch.name === "calendar");
-
-			channel.send(msg)
-			.then(message => console.log(`${message.content}`))
-			.catch(console.error);
+			/* !!!!!!!!! */
+			channelInstance.send(msg);
+			//console.log("Sent to " + channelInstance.name + "?");
+			//.then(message => console.log(`${message.content}`))
+			//.catch(console.error);
 
 		})
 	}).on('error', (e) => {
@@ -104,13 +101,53 @@ function execute() {
 		"m": today.getMinutes()
 	}
 
-	//console.log("current time: " + now.h + ":" + now.m + " sunrise is: " + sunrise.h + ":" + sunrise.m);
-
+	/* AT SUNRISE: */
 	if (sunrise.h == now.h && sunrise.m == now.m) {
-		dailymessage();
+
+		const guildsCache = client.guilds.cache;
+		// Returns an Iterable collection of servers the bot is a part of
+		// each item is an object of type GUILD
+		// (https://discord.js.org/#/docs/discord.js/stable/class/Guild)
+
+		let success = 0;
+		let remind = 0;
+		let fail = 0;
+		//code used from: https://stackoverflow.com/questions/63261585/how-to-send-a-message-to-all-servers
+		guildsCache.each(guild => {
+			try {
+				const channel = guild.channels.cache.find(channel => channel.name === 'calendar');
+				//for the first channel
+				const firstChannel = guild.channels.cache.find(firstChannel => firstChannel.isText());
+				if (channel) {
+					//console.log("Sent a message to the calendar channel in " + guild.name);
+					dailymessage(channel);
+					success++;
+				} else {
+					//console.log('The server ' + guild.name + ' has no channels named calendar.');
+					//dailymessage(firstChannel);
+					firstChannel.send("It is recommended that you create a channel called #calendar for this bot to send messages in.  Thanks :)");
+					//console.log("In the " + guild.name + " server, message was sent to " + firstChannel.name);
+					remind++;
+				}
+			} catch (err) {
+				//console.log('Could not send message to ' + guild.name + '.');
+				fail++;
+			}
+		});
+
+		//print status at sunrise
+		console.log(success + " sent, " + remind + " reminded, " + fail + " failed.");
+
 	}
-	
 }
 
-const x = 59000; //check for sunrise every 59 seconds
-setInterval(function() { execute(); }, x);
+	//interval in seconds the program should check for sunrise
+	const interval = 60
+	setInterval(function() { execute(); }, (interval*1000));
+
+  /*
+	*
+	* Thank you to Brian Glass at https://orthocal.info/api
+	* for providing this Orthodox Calendar API!
+	*
+	*/
